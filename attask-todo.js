86a044e3323b38b1aa5ce90898a6267f02ce7897
joeljohnson <joeljohnson@attask.com>
2013-05-09 20:37:17 -0600

    initial commit

---
+++ attask-todo.js
@@ -0,0 +1,52 @@
+#!/usr/local/bin/node
+
+var request = require('request');
+
+var username = process.argv[2];
+var password = process.argv[3];
+var todos = process.argv.slice(4);
+
+var loginOptions = {
+	method: 'POST',
+	qs: {
+		username: username,
+		password: password
+	},
+	json: true
+};
+
+request('https://hub.attask.com/attask/api-internal/login', loginOptions, function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+		var sessionID = body.data.sessionID;
+		var userID = body.data.userID;
+
+		console.log('logged in as', username, 'with session ID', sessionID);
+
+		todos.forEach(function(todo) {
+			todo = eval('('+todo+')');
+			todo.sessionID = sessionID;
+			if(todo.projectID === undefined) {
+				todo.personal = true;
+			}
+			if(todo.assignedToID === undefined) {
+				todo.assignedToID = userID;
+			}
+
+			var newTaskOptions = {
+				method: 'POST',
+				qs: todo,
+				json: true
+			};
+
+			request('https://hub.attask.com/attask/api-internal/task', newTaskOptions, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					console.log("created task", body.ID);
+				} else {
+					console.error(response);
+				}
+			});
+		});
+	} else {
+		console.error(response);
+	}
+});
